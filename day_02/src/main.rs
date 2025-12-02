e487d913930d25d85fad4989e36ca203aebffc21
Daniel Hollosi <ravendan@gmail.com>
2025-12-02 10:55:34 +0100

    Day 2-2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,28 +1,45 @@
+#![allow(dead_code)]
+
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 
-fn check_invalid_ids_v1(id_start: &str, id_end: &str) -> u64 {
+fn check_invalid_ids(id_start: &str, id_end: &str) -> u64 {
     let mut invalids: HashSet<u64> = HashSet::new();
     let num_start = id_start.parse::<u64>().expect("Couldn't parse start ID");
     let num_end = id_end.parse::<u64>().expect("Couldn't parse end ID");
     for i in num_start..num_end {
-        let i_string = i.to_string();
-        let chunks = i_string.split_at_checked(i_string.len() / 2);
-        match chunks {
-            Some((half1, half2)) => {
-                if half1 == half2 {
-                    invalids.insert(i);
-                }
-            },
-            None => {
-                continue;
-            }
-        };
+        if check_id_v2(&i) {
+            invalids.insert(i);
+        }
     }
 
     invalids.into_iter().sum()
+}
+
+fn check_id_v1(id: &u64) -> bool {
+    let id_string = id.to_string();
+    if let Some((half1, half2)) = id_string.split_at_checked(id_string.len() / 2) {
+        return half1 == half2;
+    }
+    false
+}
+
+fn check_id_v2(id: &u64) -> bool {
+    let id_string = id.to_string();
+    for i in 1..(id_string.len() / 2 + 1) {
+        let pattern = &id_string[0..i];
+        let size = pattern.len();
+        let mut ptr = i;
+        while ptr + size <= id_string.len() && &id_string[ptr..(ptr + size)] == pattern {
+            ptr += size;
+        }
+        if ptr == id_string.len() {
+            return true
+        }
+    }
+    false
 }
 
 fn main() {
@@ -32,10 +49,10 @@ fn main() {
 
     while reader.read_until(b',', &mut buf).expect("File read failed") != 0 {
         let mut chars = buf.clone();
-        chars.pop();
+        chars.pop(); // remove comma from string
         let pair = String::from_utf8(chars).unwrap();
         let (id_start, id_end) = pair.split_once('-').expect("Invalid ID pair");
-        invalids += check_invalid_ids_v1(id_start, id_end);
+        invalids += check_invalid_ids(id_start, id_end);
 
         buf.clear();
     }
