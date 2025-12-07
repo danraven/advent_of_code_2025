98e811c67b8c1ecd238f0fb48b79bc084a8f25af
Daniel Hollosi <ravendan@gmail.com>
2025-12-07 18:20:00 +0100

    Day 7-2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
@@ -9,23 +9,26 @@ const FILE_INPUT: &str = "input.txt";
 
 fn main() {
     let mut splits: u32 = 0;
-    let mut tachyons: HashSet<usize> = HashSet::new();
+    let mut tachyons: BTreeMap<usize, u64> = BTreeMap::new();
     let file = File::open(FILE_INPUT).unwrap();
 
     for line in BufReader::new(file).lines() {
         for (idx, chr) in line.unwrap().chars().enumerate() {
             if chr == CHAR_START {
-                tachyons.insert(idx);
+                tachyons.insert(idx, 1);
                 continue;
             }
-            if chr == CHAR_SPLIT && tachyons.contains(&idx) {
+            if chr == CHAR_SPLIT && tachyons.contains_key(&idx) {
+                let parent = tachyons[&idx].clone();
+                *tachyons.entry(idx - 1).or_insert(0) += parent;
+                *tachyons.entry(idx + 1).or_insert(0) += parent;
                 tachyons.remove(&idx);
-                tachyons.insert(idx - 1);
-                tachyons.insert(idx + 1);
                 splits += 1;
             }
         }
     }
+    let timelines: u64 = tachyons.values().sum();
 
     println!("There are {} splits", splits);
+    println!("There are {} timelines", timelines);
 }
